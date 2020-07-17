a1a7c84713fb3aa2a37d4b602cf67daf55466a57
ahmedSalah-11 <ahmedsalah@stud.cu.edu.eg>
2020-07-17 14:54:16 +0200

    Updates

---
+++ precache-manifest.ebc8f24b1303e9c50bec7c6c1a92529c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf87f2f36141357b774660d7e78de84",
+    "url": "/Corona_tracker/index.html"
+  },
+  {
+    "revision": "f7b63ff66fd2616891cd",
+    "url": "/Corona_tracker/static/css/main.78382815.chunk.css"
+  },
+  {
+    "revision": "626719a027f567242d38",
+    "url": "/Corona_tracker/static/js/2.0e1fbe36.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona_tracker/static/js/2.0e1fbe36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b63ff66fd2616891cd",
+    "url": "/Corona_tracker/static/js/main.369d79b2.chunk.js"
+  },
+  {
+    "revision": "e6714209971d5afba6aa",
+    "url": "/Corona_tracker/static/js/runtime-main.24bb3864.js"
+  },
+  {
+    "revision": "c53f729a78a08e8d05141efc69b62f0e",
+    "url": "/Corona_tracker/static/media/img.c53f729a.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Corona_tracker/precache-manifest.ebc8f24b1303e9c50bec7c6c1a92529c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Corona_tracker/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
